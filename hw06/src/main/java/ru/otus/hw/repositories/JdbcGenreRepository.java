21559c6562a068014c381aa7be9648c5e7e0c43e
Валерия <lera.evstratova@gmail.com>
2024-07-10 10:02:58 +0300

    hw06 перенос предыдущего ДЗ

---
+++ hw06/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -0,0 +1,42 @@
+package ru.otus.hw.repositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Genre;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Repository
+public class JdbcGenreRepository implements GenreRepository {
+
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    @Override
+    public List<Genre> findAll() {
+        return namedParameterJdbcOperations.query("select id, name from genres", new GenreRowMapper());
+    }
+
+    @Override
+    public List<Genre> findAllByIds(Set<Long> ids) {
+        return namedParameterJdbcOperations.query(
+                "select id, name from genres where id in (:ids)", Map.of("ids", ids), new GenreRowMapper()
+        );
+    }
+
+    private static class GenreRowMapper implements RowMapper<Genre> {
+
+        @Override
+        public Genre mapRow(ResultSet rs, int i) throws SQLException {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            return new Genre(id, name);
+        }
+    }
+}
