06ed8fe9c81f9a999fad5c6964d609f371015c79
Валерия <lera.evstratova@gmail.com>
2024-10-22 22:24:36 +0300

    hw15

--- hw15/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++ hw15/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -1,7 +1,13 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Author;
 
+@RepositoryRestResource(path = "author")
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @RestResource(path = "fullNames", rel = "fullNames")
+    Author findByFullName(String fullName);
 }
--- hw15/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw15/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,10 +1,18 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "comment")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @RestResource(path = "bookTitles", rel = "bookTitles")
+    List<Comment> findByBookTitle(String bookTitle);
+
+    @RestResource(exported = false)
     List<Comment> findByBookId(long bookId);
 }
